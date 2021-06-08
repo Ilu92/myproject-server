b7c0777ea95a5cd32ffc7f097020b6589068e6fc
ilya <ziluha491@gmail.com>
2021-06-08 14:41:14 +0300

    customization UserLoginForm

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def login(request):
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
-            user = auth.authenticate(username = username, password = password)
+            user = auth.authenticate(username = username, password= password)
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
